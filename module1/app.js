f7040443edb8fe2751c589898192c5c356d9aa40
Earl Lewis <earl.lewis.1@gmail.com>
2016-10-14 15:39:35 -0600

    refactored for $inject

--- module1/app.js
+++ module1/app.js
@@ -1,15 +1,28 @@
-//
+// use the IIFE pattern "immediately invoked function expression"
 (function () {
 'use strict';
 
 angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
 
-.controller('LunchCheckController', function ($scope) {
-  $scope.checkItems = function(lunch_items) {
-    var count = lunch_items.split();
-    return lunch_items.split().count();
-  }
+LunchCheckController.$inject = ['$scope'];
 
-});
+function LunchCheckController($scope) {
+  $scope.message = 'You must be hungry. Add something to your menu!'
+  $scope.count_items = 0;
+
+  $scope.checkItems = function() {
+    if ($scope.lunch_items === undefined) {
+      return null;
+    } else {
+      var splits = $scope.lunch_items.split(',');
+      if (splits.length <= 3) {
+        $scope.message = 'Enjoy!'
+      } else if (splits.length > 3) {
+        $scope.message = 'Too much!'
+      }
+    }
+  };
+}
 
 })();
