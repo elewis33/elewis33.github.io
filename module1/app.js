b0861debe85d9504ae3d4ec16bcb422826376ca5
Earl Lewis <earl.lewis.1@gmail.com>
2016-10-14 08:47:31 -0600

    updated index.html and app.js

--- module1/app.js
+++ module1/app.js
@@ -1,5 +1,15 @@
-var app = angular.module('LunchCheck', []);
-app.controller('checkLunch', function($scope) {
-    $scope.firstName= "John";
-    $scope.lastName= "Doe";
+//
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+
+.controller('LunchCheckController', function ($scope) {
+  $scope.checkItems = function(lunch_items) {
+    var count = lunch_items.split();
+    return lunch_items.split().count();
+  }
+
 });
+
+})();
