6f8ecdc1b2014ba7ccc3e5ef7cb3d49ac8a50457
Earl Lewis <earl.lewis.1@gmail.com>
2016-10-28 13:23:34 -0600

    update project

--- module3/app.js
+++ module3/app.js
@@ -7,32 +7,63 @@ angular.module('NarrowItDownApp', [])
 .directive('foundItems', FoundItemsDirective)
 .constant('ApiPath', "https://davids-restaurant.herokuapp.com/menu_items.json");
 
+function FoundItemsDirective() {
+  var ddo = {
+    templateUrl: 'foundItems.html',
+    scope: {
+      items: '<',
+      myTitle: '@title',
+      badRemove: '=',
+      onRemove: '&'
+    },
+    controller: FoundItemsDirectiveController,
+    controllerAs: 'list',
+    bindToController: true
+  };
+
+  return ddo;
+}
+
+function FoundItemsDirectiveController() {
+  var list = this;
+
+  list.itemsInList = function () {
+    for (var i = 0; i < list.items.length; i++) {
+      var name = list.items[i].name;
+      if (name.toLowerCase().indexOf("cookie") !== -1) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+}
 
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
   var menu = this;
-  var menu.searchTerm = '';
+  menu.searchTerm = '';
 
-  var promise = MenuSearchService.getMatchedMenuItems();
+  var promise = MenuSearchService.getMenuItems();
 
   promise.then(function (response) {
-    menu.data = response.data;
-    console.log(menu.data);
+    menu.items = response.data.menu_items;
+    console.log(menu.items);
   })
   .catch(function (error) {
     console.log("Something went terribly wrong.");
   });
 
-  menu.logMenuItems = function (searchTerm) {
-    var promise = MenuSearchService.getMatchedMenuItems();
-
-    promise.then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-  };
+  // menu.logMenuItems = function () {
+  //   var promise = MenuSearchService.getMenuItems();
+  //
+  //   promise.then(function (response) {
+  //     console.log(response.data);
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   })
+  // };
 
 }
 
@@ -42,8 +73,8 @@ function MenuSearchService($http, ApiPath) {
   var service = this;
   service.found = [];
 
-  service.getMatchedMenuItems = function (menu.searchTerm) {
-    console.log('running getMatchedMenuItems');
+  service.getMenuItems = function () {
+    console.log('running getMenuItems');
     var response = $http({
       method: "GET",
       url: (ApiPath)
